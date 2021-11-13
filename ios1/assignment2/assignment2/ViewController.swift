618d3e97e2443678d0eae3093ef8b881cce24bab
ZeK1ng <dgogi17@freeuni.edu.ge>
2021-11-13 12:39:09 +0400

    shwiftry

--- ios1/assignment2/assignment2/ViewController.swift
+++ ios1/assignment2/assignment2/ViewController.swift
@@ -9,7 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var txt4: UITextField!
+    @IBOutlet var nm: UITextField!
+    @IBOutlet var txt3: UITextField!
+    @IBOutlet var txt2: UITextField!
     @IBOutlet var getSwifty: UITextField!
+
+    @IBOutlet var midStackv: UIStackView!
+    @IBOutlet var txt13: UITextField!
+    @IBOutlet var txt12: UITextField!
+    @IBOutlet var txt11: UITextField!
+    @IBOutlet var txt1: UITextField!
     @IBOutlet var djrick: UITextField!
     @IBOutlet var profPic: UIImageView!
     override func viewDidLoad() {
@@ -17,9 +27,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
        // self.view.backgroundColor = UIColor(patternImage: UIImage(named: "retrowave.jpg")!)
         profPic.layer.cornerRadius = profPic.frame.size.width/2
+        midStackv.layer.cornerRadius = 16
         profPic.clipsToBounds = true
         djrick.borderStyle = .none
         getSwifty.borderStyle = .none
+        nm.borderStyle = .none
     }
 
 
